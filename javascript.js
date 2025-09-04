d6abb7c80f8c723d33ed8dd3f876569ed7b4d535
Winifred Rex <77475621+winifredrex@users.noreply.github.com>
2025-09-04 14:00:27 +0100

    my second commit for java

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const name = confirm("What is your name?") // this uses booolean value
+console.log(name)
+
+let jobtitle = prompt("What is your Job Tile?") // this uses string values and returns what user types in
+console.log(jobtitle)
