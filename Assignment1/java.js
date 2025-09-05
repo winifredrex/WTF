fc6937030a85a5daa60bd85df9ac7a4da5136e96
Winifred Rex <77475621+winifredrex@users.noreply.github.com>
2025-09-05 12:24:03 +0100

    comments

--- Assignment1/java.js
+++ Assignment1/java.js
@@ -1,14 +1,14 @@
-const num1 = 2
-const num2 = 3
+const num1 = 2 // creating a constant variable that accepts a number value
+const num2 = 3 //creating a constant variable that accepts a number value
 
-let name = "winifred" // this uses string values and returns what user types in
+let name = "winifred" //this accepts a string value
 
-const sum = num1 + num2
-const subtraction = num2 - num1
-const multiplication = num1 * num2
-const division = num2 / num1
+const sum = num1 + num2 // creating a constant variable that sums num1 and num2
+const subtraction = num2 - num1 // creating a constant variable that subtracts num1 from num2
+const multiplication = num1 * num2 // creating a constant variable that multiplies num1 and num2
+const division = num2 / num1 // creating a constant variable that divides num2 by num1
 
-console.log("Hello " + name + ", the sum of " + num1 + " and " + num2 + " is " + sum)
-console.log("Hello " + name + ", the subtraction of " + num2 + " and " + num1 + " is " + subtraction)
-console.log("Hello " + name + ", the multiplication of " + num1 + " and " + num2 + " is " + multiplication)
-console.log("Hello " + name + ", the division of " + num2 + " and " + num1 + " is " + division)
+console.log("Hello " + name + ", the sum of " + num1 + " and " + num2 + " is " + sum) // prints out the sum of num1 and num2 in a sentence on the console
+console.log("Hello " + name + ", the subtraction of " + num2 + " and " + num1 + " is " + subtraction) // prints out the subtraction of num1 from num2 in a sentence on the console
+console.log("Hello " + name + ", the multiplication of " + num1 + " and " + num2 + " is " + multiplication) // prints out the multiplication of num1 and num2 in a sentence on the console
+console.log("Hello " + name + ", the division of " + num2 + " and " + num1 + " is " + division) // prints out the division of num2 by num1 in a sentence on the console
