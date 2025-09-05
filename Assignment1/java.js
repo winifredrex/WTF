7b063efa206338b344a5635d7893e755c324a226
Winifred Rex <77475621+winifredrex@users.noreply.github.com>
2025-09-05 12:21:22 +0100

    my assignment

---
+++ Assignment1/java.js
@@ -0,0 +1,14 @@
+const num1 = 2
+const num2 = 3
+
+let name = "winifred" // this uses string values and returns what user types in
+
+const sum = num1 + num2
+const subtraction = num2 - num1
+const multiplication = num1 * num2
+const division = num2 / num1
+
+console.log("Hello " + name + ", the sum of " + num1 + " and " + num2 + " is " + sum)
+console.log("Hello " + name + ", the subtraction of " + num2 + " and " + num1 + " is " + subtraction)
+console.log("Hello " + name + ", the multiplication of " + num1 + " and " + num2 + " is " + multiplication)
+console.log("Hello " + name + ", the division of " + num2 + " and " + num1 + " is " + division)
